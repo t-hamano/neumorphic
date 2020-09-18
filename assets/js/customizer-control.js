b6e4f34140c70cfc61f3ec5551a1363ddca1a919
Tetsuaki Hamano <tetsuaki.hamano@gmail.com>
2020-09-18 12:58:40 +0900

    Fix: Problem about customizer color settings are not reflected

--- assets/js/customizer-control.js
+++ assets/js/customizer-control.js
@@ -3,23 +3,8 @@
  */
 
 ( function( $ ) {
+
 	wp.customize.bind( 'ready', function() {
-
-		// Set default color for each color based on current color skin.
-		var skinName = _wpCustomizeSettings.settings.color_skin.value;
-
-		for ( var controlName in neumorphicSkinList[ skinName ]) {
-			var control = wp.customize.control.instance( 'color_' + controlName );
-			picker = control.container.find( '.color-picker-hex' );
-			control.setting.set( neumorphicSkinList[ skinName ][ controlName ]);
-
-			picker.val( control.setting() );
-			picker.wpColorPicker( 'defaultColor', control.setting() );
-			picker.attr({
-				'placeholder': control.setting(),
-				'data-default-color': control.setting()
-			});
-		}
 
 		// Set each color based on the selected color skin.
 		wp.customize( 'color_skin', function( value ) {
