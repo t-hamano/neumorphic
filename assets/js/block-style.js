d153571a285d34997a2842765d6e2330ee6941e6
Tetsuaki Hamano / 浜野 哲明 <tetsuaki.hamano@gmail.com>
2021-08-22 05:24:42 +0000

    Styling: Archives / Calendar / Categories block

--- assets/js/block-style.js
+++ assets/js/block-style.js
@@ -2,80 +2,38 @@
  * Scripts for block editor.
  */
 
+/* eslint-disable prettier/prettier */
 wp.domReady( function () {
-	// Add custom style
-	wp.blocks.registerBlockStyle( 'core/heading', {
-		name: 'nm-none',
-		label: 'No Neumorphic',
-		isDefault: false,
-	} );
+	const styles = [
+		// Text
+		{ block: 'core/heading',      name: 'nm-none',    label: 'No Neumorphic',  isDefault: false },
+		{ block: 'core/quote',        name: 'nm-none',    label: 'No Neumorphic',  isDefault: false },
+		{ block: 'core/verse',        name: 'nm-none',    label: 'No Neumorphic',  isDefault: false },
+		{ block: 'core/table',        name: 'nm-none',    label: 'No Neumorphic',  isDefault: false },
+		// Media
+		{ block: 'core/image',        name: 'nm-none',    label: 'No Neumorphic',  isDefault: false },
+		{ block: 'core/cover',        name: 'nm',         label: 'Neumorphic',     isDefault: false },
+		{ block: 'core/cover',        name: 'nm-reverse', label: 'Neumorphic Rev', isDefault: false },
+		{ block: 'core/media-text',   name: 'nm',         label: 'Neumorphic',     isDefault: false },
+		// Design
+		{ block: 'core/columns',      name: 'nm',         label: 'Neumorphic',     isDefault: false },
+		{ block: 'core/columns',      name: 'nm-reverse', label: 'Neumorphic Rev', isDefault: false },
+		{ block: 'core/group',        name: 'nm',         label: 'Neumorphic',     isDefault: false },
+		{ block: 'core/group',        name: 'nm-reverse', label: 'Neumorphic Rev', isDefault: false },
+		// Widget
+		{ block: 'core/archives',     name: 'nm',         label: 'Neumorphic',     isDefault: false },
+		{ block: 'core/archives',     name: 'nm-reverse', label: 'Neumorphic Rev', isDefault: false },
+		{ block: 'core/categories',   name: 'nm',         label: 'Neumorphic',     isDefault: false },
+		{ block: 'core/categories',   name: 'nm-reverse', label: 'Neumorphic Rev', isDefault: false },
+		{ block: 'core/social-links', name: 'nm',         label: 'Neumorphic',     isDefault: false },
+	];
 
-	wp.blocks.registerBlockStyle( 'core/image', {
-		name: 'nm-none',
-		label: 'No Neumorphic',
-		isDefault: false,
-	} );
-
-	wp.blocks.registerBlockStyle( 'core/quote', {
-		name: 'nm-none',
-		label: 'No Neumorphic',
-		isDefault: false,
-	} );
-
-	wp.blocks.registerBlockStyle( 'core/verse', {
-		name: 'nm-none',
-		label: 'No Neumorphic',
-		isDefault: false,
-	} );
-
-	wp.blocks.registerBlockStyle( 'core/table', {
-		name: 'nm-none',
-		label: 'No Neumorphic',
-		isDefault: false,
-	} );
-
-	wp.blocks.registerBlockStyle( 'core/cover', {
-		name: 'nm',
-		label: 'Neumorphic',
-		isDefault: false,
-	} );
-	wp.blocks.registerBlockStyle( 'core/cover', {
-		name: 'nm-reverse',
-		label: 'Neumorphic Rev',
-		isDefault: false,
-	} );
-
-	wp.blocks.registerBlockStyle( 'core/media-text', {
-		name: 'nm',
-		label: 'Neumorphic',
-		isDefault: false,
-	} );
-
-	wp.blocks.registerBlockStyle( 'core/columns', {
-		name: 'nm',
-		label: 'Neumorphic',
-		isDefault: false,
-	} );
-	wp.blocks.registerBlockStyle( 'core/columns', {
-		name: 'nm-reverse',
-		label: 'Neumorphic Rev',
-		isDefault: false,
-	} );
-
-	wp.blocks.registerBlockStyle( 'core/group', {
-		name: 'nm',
-		label: 'Neumorphic',
-		isDefault: false,
-	} );
-	wp.blocks.registerBlockStyle( 'core/group', {
-		name: 'nm-reverse',
-		label: 'Neumorphic Rev',
-		isDefault: false,
-	} );
-
-	wp.blocks.registerBlockStyle( 'core/social-links', {
-		name: 'nm',
-		label: 'Neumorphic',
-		isDefault: false,
-	} );
+	styles.forEach(function( style ){
+		wp.blocks.registerBlockStyle( style.block, {
+			name: style.name,
+			label: style.label,
+			isDefault: style.isDefault,
+		} );
+	});
 } );
+
